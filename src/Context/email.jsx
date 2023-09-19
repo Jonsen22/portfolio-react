732281210f422977820a5a8d20c72a6b2eeced8e
Jonsen22 <gabriel.jansen222@gmail.com>
2023-09-19 13:03:57 -0300

    Production test2

--- src/Context/email.jsx
+++ src/Context/email.jsx
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-const API_URL = process.env.EMAIL_URL;
 
 // const API_URL = "https://localhost:7297/";
 
 export async function postEmail(name, email, message) {
   try {
+    const API_URL = process.env.EMAIL_URL;
+    console.log(API_URL)
     const response = await axios.post(API_URL+"api/Email", { name, email, message });
     console.log(response);
     return response.data;
